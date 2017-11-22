8111fcdbb174ad4a1a9d2125640269dad9ad8bf2
b-sirius <padfoot_07@163.com>
2017-11-22 20:06:02 +0800

    almost done

--- IFE-test-25/tree_pro.js
+++ IFE-test-25/tree_pro.js
@@ -1,9 +1,9 @@
 'use strict';
-let _ = {
+const _ = {
     newEL(elType, className = '', option = {}) {
         let el = document.createElement(elType);
         el.className = className;
-        
+
         for (let item of Object.keys(option)) {
             el[item] = option[item];
         }
@@ -15,61 +15,65 @@ let _ = {
 const file = Symbol('file');
 const folder = Symbol('folder');
 
-// 标记状态
-const marked = Symbol('marked');
-const toBeMarked = Symbol('toBeMarked');
-const unmarked = Symbol('unmarked');
-
 // 文件夹状态
 const expanded = Symbol('expanded');
 const collapsed = Symbol('collapsed');
 
 // 基本节点
-let Node = {
+const Node = {
     _init(data, type) {
         this.data = data;
         this.type = type;
-        this.markState = unmarked;
+        this.marked = false;
     },
-    setParent(p) {
-        if (Object.getPrototypeOf(p) === FolderNode) {
-            this.parent = p;
-        }
-    },
-    marked() {
-        this.markState = marked;
-    },
-    toBeMarked() {
-        this.markState = toBeMarked;
-    },
-    unmarked() {
-        this.markState = unmarked;
+    toggleMark() {
+        this.marked = !this.marked;
     },
 };
 
 // 叶子节点
-let FileNode = Object.create(Node);
+const FileNode = Object.create(Node);
 Object.assign(FileNode, {
     build(data = '新文件') {
         this._init(data, file);
-        this.parent = null;
+        this.nodeEls = null;
     },
+    setNodeEls({
+        nodeBodyEl,
+        markBtn,
+        markEl
+    }) {
+        this.nodeEls = {
+            nodeBodyEl,
+            markBtn,
+            markEl
+        }
+    }
 });
 
 // 分支节点
-let FolderNode = Object.create(Node);
+const FolderNode = Object.create(Node);
 Object.assign(FolderNode, {
     build(data = '新文件夹', childList = []) { // 初始化
         this._init(data, folder);
-        this.parent = null;
         this.childList = [];
-        this.expended = true;
+        this.expanded = true;
+        this.nodeEls = null;
         this.add(childList);
-        this._contactChildList();
     },
-    _contactChildList() { // 建立子节点与父节点的联系
-        for (let child of this.childList) {
-            child.setParent(this);
+    search(compareFn) {
+        const quque = [this];
+
+        while (quque.length) {
+            let current = quque.pop();
+
+            if (compareFn(current))
+                return current;
+
+            if (current.childList) {
+                for (let item of current.childList)
+                    quque.push(item);
+            }
         }
     },
     isNode(f) { // 判断是否是节点
@@ -95,51 +99,131 @@ Object.assign(FolderNode, {
             return;
         }
     },
-    toggle() {
-        this.expended = !this.expanded;
-    }
+    setNodeEls({
+        nodeBodyEl,
+        markBtn,
+        markEl,
+        switchBtn
+    }) {
+        this.nodeEls = {
+            nodeBodyEl,
+            markBtn,
+            markEl,
+            switchBtn
+        }
+    },
+    toggleExpand() {
+        this.expanded = !this.expanded;
+    },
 });
 
-let render = {
-    [folder]: function(node) {
+const render = {
+    [folder]: function (node) {
         let nodeBody = _.newEL('div', 'node-body');
         let nodeHeader = _.newEL('label', 'node-header');
-        let arrBtn = _.newEL('img', 'arrow arrow-right', {src: './imgs/arrow.svg'});
-        let folderImg = _.newEL('img', 'folder', {src: './imgs/folder.svg'});
-        let nameEl = _.newEL('span', undefined, {textContent: node.data});
-        let markEl = _.newEL('div', 'to-mark');
+        let arrBtn = _.newEL('a', 'arrow arrow-down', {
+            href: 'javascript:'
+        });
+        let folderImg = _.newEL('img', 'folder', {
+            src: './imgs/folder.svg'
+        });
+        let nameEl = _.newEL('span', undefined, {
+            textContent: node.data
+        });
+        let markEl = _.newEL('div', 'mark');
 
         nodeBody.appendChild(nodeHeader);
         nodeHeader.appendChild(arrBtn);
         nodeHeader.appendChild(folderImg);
         nodeHeader.appendChild(nameEl);
         nodeHeader.appendChild(markEl);
-        
-        arrBtn.addEventListener('click', node.toggle);
-        
+
+        // 与状态切换直接相关的元素
+        let nodeEls = {
+            nodeBodyEl: nodeBody,
+            switchBtn: arrBtn,
+            markBtn: nodeHeader,
+            markEl: markEl,
+        }
+        // 与节点关联
+        node.setNodeEls(nodeEls);
+
         for (let item of node.childList) {
             let childNodeBody = render[item.type](item);
+
             nodeBody.appendChild(childNodeBody);
         }
 
+        nodeEls.switchBtn.addEventListener('click', () => {
+            nodeController.toggleExpand(node);
+        });
+
+        nodeEls.markBtn.addEventListener('click', (e) => {
+            // 如果点击的是箭头
+            if (e.target === nodeEls.switchBtn)
+                return;
+
+            let lastMarkedNode = nodeController.getLastMarkedNode();
+            if (lastMarkedNode !== null) {
+                nodeController.toggleMark(lastMarkedNode);
+                // 重复点击自身
+                if (lastMarkedNode === node) {
+                    nodeController.setLastMarkedNode(null);
+                    return;
+                }
+            }
+
+            nodeController.toggleMark(node);
+            nodeController.setLastMarkedNode(node);
+        });
+
         return nodeBody;
     },
-    [file]: function(node) {
+    [file]: function (node) {
         let nodeBody = _.newEL('div', 'node-body');
         let nodeContent = _.newEL('label', 'node');
-        let fileImg = _.newEL('img', 'file', {src: './imgs/file.svg'});
-        let nameEl = _.newEL('span', undefined, {textContent: node.data});
-        let markEl = _.newEL('div', 'to-mark');
+        let fileImg = _.newEL('img', 'file', {
+            src: './imgs/file.svg'
+        });
+        let nameEl = _.newEL('span', undefined, {
+            textContent: node.data
+        });
+        let markEl = _.newEL('div', 'mark');
+
+        // 与状态切换直接相关的元素
+        let nodeEls = {
+            nodeBodyEl: nodeBody,
+            markBtn: nodeContent,
+            markEl: markEl,
+        }
+        // 与节点关联
+        node.setNodeEls(nodeEls);
 
         nodeBody.appendChild(nodeContent);
         nodeContent.appendChild(fileImg);
         nodeContent.appendChild(nameEl);
         nodeContent.appendChild(markEl);
 
+        nodeEls.markBtn.addEventListener('click', () => {
+            let lastMarkedNode = nodeController.getLastMarkedNode();
+            if (lastMarkedNode !== null) {
+                nodeController.toggleMark(lastMarkedNode);
+                // 重复点击自身
+                if (lastMarkedNode === node) {
+                    nodeController.setLastMarkedNode(null);
+                    return;
+                }
+            }
+
+            nodeController.toggleMark(node);
+            nodeController.setLastMarkedNode(node);
+        });
+
         return nodeBody;
     }
 }
 
+//=============demo代码==================
 let root = Object.create(FolderNode);
 root.build('科幻作品');
 let folder1 = Object.create(FolderNode);
@@ -151,5 +235,123 @@ folder1.add(child1);
 root.add(folder1);
 
 let rootBody = render[folder](root);
-let rootEl = document.querySelector('#js-root');
+const rootEl = document.querySelector('#js-root');
 rootEl.appendChild(rootBody);
+
+//=============功能================
+let nodeController = {
+    createFile(data) {
+        let node = Object.create(FileNode);
+        node.build(data);
+        return node;
+    },
+    createFolder(data) {
+        let node = Object.create(FolderNode);
+        node.build(data);
+        return node;
+    },
+    toggleMark(node) {
+        node.toggleMark();
+        node.nodeEls.markEl.classList.toggle('marked');
+
+        // 记录当前被选中的节点
+        this.lastMarkedNode = node;
+    },
+    toggleExpand(node) {
+        node.toggleExpand();
+        node.nodeEls.switchBtn.classList.toggle('arrow-down');
+        node.nodeEls.switchBtn.classList.toggle('arrow-right');
+        for (let item of node.childList) {
+            item.nodeEls.nodeBodyEl.classList.toggle('hidden');
+        }
+    },
+    addTo(toNode, newNode) {
+        toNode.add(newNode);
+
+        let newNodeBody = render[newNode.type](newNode);
+        toNode.nodeEls.nodeBodyEl.appendChild(newNodeBody);
+    },
+    setLastMarkedNode(node) {
+        this._lastMarkedNode = node;
+    },
+    getLastMarkedNode() {
+        return this._lastMarkedNode;
+    },
+    _lastMarkedNode: null,
+}
+
+let addToRoot = (() => {
+    let createNode = {
+        [file]: function (data) {
+            return nodeController.createFile(data);
+        },
+        [folder]: function (data) {
+            return nodeController.createFolder(data);
+        }
+    }
+
+    return (type) => {
+        let node = root.search((node) => {
+            return node.marked;
+        });
+
+        if (node === undefined) {
+            alert('未选择目录');
+            return;
+        }
+
+        if (node.type !== folder) {
+            alert('只可向文件夹内添加文件');
+            return;
+        }
+
+        let data = prompt('请输入的名称');
+        if (data.trim() === '') {
+            alert('名称不可为空');
+            return;
+        }
+
+        if (!node.expanded) {
+            nodeController.toggleExpand(node);
+        }
+
+        let newNode = createNode[type](data);
+        nodeController.addTo(node, newNode);
+    
+    }
+})();
+
+const addFileBtn = document.querySelector('#js-addFile');
+const addFolderBtn = document.querySelector('#js-addFolder');
+const searchBtn = document.querySelector('#js-search');
+const deleteBtn = document.querySelector('#js-delete');
+
+addFileBtn.onclick = () => {
+    addToRoot(file);
+}
+
+addFolderBtn.onclick = () => {
+    addToRoot(folder);
+}
+
+searchBtn.onclick = () => {
+    let data = prompt('请输入的名称');
+    if (data.trim() === '') {
+        alert('名称不可为空');
+        return;
+    }
+
+    let node = root.search((node) => {
+        return node.data === data;
+    });
+
+    if (node === undefined) {
+        alert('无符合条件节点');
+        return;
+    }
+
+    if (node.marked)
+        return;
+    
+    nodeController.toggleMark(node);
+}
